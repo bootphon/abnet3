401ffec8786a95662ac223bd6543cd5f176e78e0
rachine <riadrachid3@gmail.com>
2017-10-12 19:14:39 +0200

    first test loss

---
+++ test/test_loss.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 12 11:24:47 2017
+
+@author: Rachid Riad
+"""
+
+import pytest
+from abnet3.loss import coscos2, cosmargin
+import torch
+from torch.autograd import Variable
+import numpy as np
+
+losses = {
+    'coscos2': coscos2,
+    'cosmargin': cosmargin
+    }
+
+params = [a for a in losses ]
+
+@pytest.mark.parametrize('loss_func,', params)
+def test_forward(loss_func):
+    N_batch = 16
+    x1 = Variable(torch.randn(N_batch, 10))
+    x2 = Variable(torch.randn(N_batch, 10))
+    y = Variable(torch.from_numpy(np.random.choice([1,-1],N_batch)))
+    loss = losses[loss_func]()
+    res = loss.forward(x1,x2,y)
+    assert res.dim() == 1, 'fail for {}'.format(loss_func)
+
