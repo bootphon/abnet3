c890dca9e374ae75fe86b6cd4931bfde547bb58b
rachine <riadrachid3@gmail.com>
2017-11-14 14:04:25 +0100

    trainer for siamese multitask

--- abnet3/trainer.py
+++ abnet3/trainer.py
@@ -22,9 +22,9 @@ import time
 import pickle
 import os
 import matplotlib
-matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
+matplotlib.use('Agg')
 
 
 class TrainerBuilder:
@@ -35,7 +35,7 @@ class TrainerBuilder:
                  num_epochs=200, patience=20, num_max_minibatches=1000,
                  optimizer_type='SGD', lr=0.001, momentum=0.9, cuda=True,
                  seed=0):
-#        super(TrainerBuilder, self).__init__()
+        # super(TrainerBuilder, self).__init__()
         self.sampler = sampler
         self.network = network
         self.loss = loss
@@ -48,7 +48,7 @@ class TrainerBuilder:
         self.best_epoch = None
         self.seed = seed
         self.cuda = cuda
-        assert optimizer_type in ('sgd', 'adadelta','adam','adagrad')
+        assert optimizer_type in ('sgd', 'adadelta', 'adam', 'adagrad')
         if optimizer_type == 'sgd':
             self.optimizer = optim.SGD(self.network.parameters(),
                                        lr=self.lr, momentum=self.momentum)
@@ -66,56 +66,59 @@ class TrainerBuilder:
             self.network.cuda()
 
     def whoami(self):
-        return {'params':self.__dict__,
-                'network':self.network.whoami(),
-                'loss':self.loss.whoami(),
-                'sampler':self.sampler.whoami(),
+        return {'params': self.__dict__,
+                'network': self.network.whoami(),
+                'loss': self.loss.whoami(),
+                'sampler': self.sampler.whoami(),
                 'class_name': self.__class__.__name__}
 
     def save_whoami(self):
         pickle.dump(self.whoami(),
-            open(self.network.output_path+'.params',"wb" ))
+                    open(self.network.output_path+'.params', "wb"))
 
     def optimize_model(self):
         """Optimization model step
         """
         raise NotImplementedError('Unimplemented optimize_model for class:',
-                  self.__class__.__name__)
+                                  self.__class__.__name__)
 
     def train(self):
         """Train function
 
         """
         raise NotImplementedError('Unimplemented train for class:',
-                          self.__class__.__name__)
+                                  self.__class__.__name__)
 
     def plot_train_erros(self):
-        """Plot function for training losses
+        """Plot method to vizualize the train and dev errors
 
         """
-        raise NotImplementedError('Unimplemented plot_train_erros for class:',
-                          self.__class__.__name__)
+        fig = plt.figure()
+        x = range(len(self.train_losses))
+        plt.plot(x, self.train_losses, 'r-')
+        plt.plot(x, self.dev_losses, 'b+')
+        fig.savefig(self.network.output_path+"_plot.pdf",
+                    bbox_inches='tight')
 
 
 class TrainerSiamese(TrainerBuilder):
     """Siamese Trainer class for ABnet3
 
     """
-    def __init__(self,*args, **kwargs):
+    def __init__(self, *args, **kwargs):
         super(TrainerSiamese, self).__init__(*args, **kwargs)
         assert type(self.sampler) == abnet3.sampler.SamplerClusterSiamese
         assert type(self.network) == abnet3.model.SiameseNetwork
 
     def prepare_batch_from_pair_words(self, features, pairs_path,
-                                      train_mode=True,seed=0):
+                                      train_mode=True, seed=0):
         """Prepare a batch in Pytorch format based on a batch file
 
         """
 
-
-        #TODO should not be here, should be somewhere in the dataloader
-        #TODO : Encapsulate X preparation in another function
-        #TODO : Replace Numpy operation by Pytorch operation
+        # TODO should not be here, should be somewhere in the dataloader
+        # TODO : Encapsulate X preparation in another function
+        # TODO : Replace Numpy operation by Pytorch operation
         pairs = read_pairs(pairs_path)
         token_feats = {}
         for f1, s1, e1, f2, s2, e2 in pairs['same']:
@@ -129,40 +132,40 @@ class TrainerSiamese(TrainerBuilder):
             token_feats[f, s, e] = features.get(f, s, e)
         # 3. align features for each pair
         X1, X2, y = [], [], []
-        ## get features for each same pair based on DTW alignment paths
+        # get features for each same pair based on DTW alignment paths
         for f1, s1, e1, f2, s2, e2 in pairs['same']:
-            if (s1>e1) or (s2>e2):
+            if (s1 > e1) or (s2 > e2):
                 continue
             feat1 = token_feats[f1, s1, e1]
             feat2 = token_feats[f2, s2, e2]
             try:
                 path1, path2 = get_dtw_alignment(feat1, feat2)
-            except:
+            except e:
                 continue
 
-            X1.append(feat1[path1,:])
-            X2.append(feat2[path2,:])
+            X1.append(feat1[path1, :])
+            X2.append(feat2[path2, :])
             y.append(np.ones(len(path1)))
 
         for f1, s1, e1, f2, s2, e2 in pairs['diff']:
-            if (s1>e1) or (s2>e2):
+            if (s1 > e1) or (s2 > e2):
                 continue
             feat1 = token_feats[f1, s1, e1]
             feat2 = token_feats[f2, s2, e2]
             n1 = feat1.shape[0]
             n2 = feat2.shape[0]
-            X1.append(feat1[:min(n1, n2),:])
-            X2.append(feat2[:min(n1, n2),:])
+            X1.append(feat1[:min(n1, n2), :])
+            X2.append(feat2[:min(n1, n2), :])
             y.append(-1*np.ones(min(n1, n2)))
 
-        ## concatenate all features
+        # concatenate all features
         X1, X2, y = np.vstack(X1), np.vstack(X2), np.concatenate(y)
         np.random.seed(seed)
         n_pairs = len(y)
         ind = np.random.permutation(n_pairs)
         y = torch.from_numpy(y[ind])
-        X1 = torch.from_numpy(X1[ind,:])
-        X2 = torch.from_numpy(X2[ind,:])
+        X1 = torch.from_numpy(X1[ind, :])
+        X2 = torch.from_numpy(X2[ind, :])
         return X1, X2, y
 
     def get_batches(self, features, train_mode=True):
@@ -179,7 +182,7 @@ class TrainerSiamese(TrainerBuilder):
 
         batches = Parse_Dataset(batch_dir)
         num_batches = len(batches)
-        if self.num_max_minibatches<num_batches:
+        if self.num_max_minibatches < num_batches:
             selected_batches = np.random.choice(range(num_batches),
                                                 self.num_max_minibatches,
                                                 replace=False)
@@ -193,7 +196,6 @@ class TrainerSiamese(TrainerBuilder):
 
             X_batch1, X_batch2, y_batch = map(Variable, bacth_els)
             yield X_batch1, X_batch2, y_batch
-
 
     def train(self):
         """Train method to train the model
@@ -214,19 +216,19 @@ class TrainerSiamese(TrainerBuilder):
         self.network.train()
         num_batches_train = 0
         for minibatch in self.get_batches(features, train_mode=True):
-            #TODO refactor here for a step function based on specific loss
+            # TODO refactor here for a step function based on specific loss
             # enable generic train
 
             X_batch1, X_batch2, y_batch = minibatch
             if self.cuda:
                 X_batch1 = X_batch1.cuda()
                 X_batch2 = X_batch2.cuda()
-                y_batch  = y_batch.cuda()
-            emb_batch1, emb_batch2 = self.network(X_batch1,X_batch2)
+                y_batch = y_batch.cuda()
+            emb_batch1, emb_batch2 = self.network(X_batch1, X_batch2)
             train_loss_value = self.loss(emb_batch1, emb_batch2, y_batch)
             train_loss += train_loss_value.data[0]
 
-            num_batches_train +=1
+            num_batches_train += 1
 
         self.train_losses.append(train_loss)
         self.network.eval()
@@ -236,9 +238,9 @@ class TrainerSiamese(TrainerBuilder):
             if self.cuda:
                 X_batch1 = X_batch1.cuda()
                 X_batch2 = X_batch2.cuda()
-                y_batch  = y_batch.cuda()
+                y_batch = y_batch.cuda()
 
-            emb_batch1, emb_batch2 = self.network(X_batch1,X_batch2)
+            emb_batch1, emb_batch2 = self.network(X_batch1, X_batch2)
             dev_loss_value = self.loss(emb_batch1, emb_batch2, y_batch)
             dev_loss += dev_loss_value.data[0]
 
@@ -322,28 +324,256 @@ class TrainerSiamese(TrainerBuilder):
         print('Still Training but no more patience.')
         print('Finished Training')
 
-    def plot_train_erros(self):
-        """Plot method to vizualize the train and dev errors
+
+class TrainerSiameseMultitask(TrainerBuilder):
+    """Siamese Trainer class for ABnet3 for multi task phn and spk
+
+    """
+    def __init__(self, *args, **kwargs):
+        super(TrainerSiameseMultitask, self).__init__(*args, **kwargs)
+        assert type(self.sampler) == abnet3.sampler.SamplerClusterSiamese
+        assert type(self.network) == abnet3.model.SiameseMultitaskNetwork
+
+    def prepare_batch_from_pair_words(self, features, pairs_path,
+                                      train_mode=True, seed=0, fid2spk=None):
+        """Prepare a batch in Pytorch format based on a batch file
 
         """
-        fig = plt.figure()
-#        plt.gca().set_color_cycle(['red', 'blue'])
-        x = range(len(self.train_losses))
-        plt.plot(x,self.train_losses,'r-')
-        plt.plot(x,self.dev_losses,'b+')
-        fig.savefig(self.network.output_path+ "_plot.pdf",
-                    bbox_inches='tight')
+        # f are filenames, s are start times, e are end times
+        pairs = read_pairs(pairs_path)
+        token_feats = {}
+        for f1, s1, e1, f2, s2, e2 in pairs['same']:
+            token_feats[f1, s1, e1] = True
+            token_feats[f2, s2, e2] = True
+        for f1, s1, e1, f2, s2, e2 in pairs['diff']:
+            token_feats[f1, s1, e1] = True
+            token_feats[f2, s2, e2] = True
+        # 2. fill in features
+        for f, s, e in token_feats:
+            token_feats[f, s, e] = features.get(f, s, e)
+        # 3. align features for each pair
+        X1, X2, y_phn, y_spk = [], [], [], []
+        # get features for each same pair based on DTW alignment paths
+        for f1, s1, e1, f2, s2, e2 in pairs['same']:
+            if (s1 > e1) or (s2 > e2):
+                continue
+            feat1 = token_feats[f1, s1, e1]
+            feat2 = token_feats[f2, s2, e2]
+            try:
+                path1, path2 = get_dtw_alignment(feat1, feat2)
+            except e:
+                continue
+            spk1, spk2 = fid2spk[f1], fid2spk[f2]
+            if spk1 is spk2:
+                y_spk.append(np.ones(len(path1)))
+            else:
+                y_spk.append(-1*np.ones(len(path1)))
+            X1.append(feat1[path1, :])
+            X2.append(feat2[path2, :])
+            y_phn.append(np.ones(len(path1)))
 
+        for f1, s1, e1, f2, s2, e2 in pairs['diff']:
+            if (s1 > e1) or (s2 > e2):
+                continue
+            feat1 = token_feats[f1, s1, e1]
+            feat2 = token_feats[f2, s2, e2]
+            n1 = feat1.shape[0]
+            n2 = feat2.shape[0]
+            X1.append(feat1[:min(n1, n2), :])
+            X2.append(feat2[:min(n1, n2), :])
+            y_phn.append(-1*np.ones(min(n1, n2)))
+            spk1, spk2 = fid2spk[f1], fid2spk[f2]
+            if spk1 is spk2:
+                y_spk.append(np.ones(min(n1, n2)))
+            else:
+                y_spk.append(-1*np.ones(min(n1, n2)))
 
+        # concatenate all features
+        X1, X2 = np.vstack(X1), np.vstack(X2)
+        y_phn, y_spk = np.concatenate(y_phn), np.concatenate(y_spk)
+        np.random.seed(seed)
+        assert len(y_phn) == len(y_spk), 'not same number of labels...'
+        n_pairs = len(y_phn)
+        ind = np.random.permutation(n_pairs)
+        y_phn = torch.from_numpy(y_phn[ind])
+        y_spk = torch.from_numpy(y_spk[ind])
+        X1 = torch.from_numpy(X1[ind, :])
+        X2 = torch.from_numpy(X2[ind, :])
+        return X1, X2, y_phn, y_spk
+
+    def get_batches(self, features, train_mode=True):
+        """Build iteratior next batch from folder for a specific epoch
+
+        """
+
+        if train_mode:
+            batch_dir = os.path.join(self.sampler.directory_output,
+                                     'train_pairs')
+        else:
+            batch_dir = os.path.join(self.sampler.directory_output,
+                                     'dev_pairs')
+
+        batches = Parse_Dataset(batch_dir)
+        num_batches = len(batches)
+        if self.num_max_minibatches < num_batches:
+            selected_batches = np.random.choice(range(num_batches),
+                                                self.num_max_minibatches,
+                                                replace=False)
+        else:
+            print("Number of batches not sufficient," +
+                  " iterating over all the batches")
+            selected_batches = np.random.permutation(range(num_batches))
+        for idx in selected_batches:
+            bacth_els = self.prepare_batch_from_pair_words(
+                    features, batches[idx], train_mode=train_mode)
+
+            X_batch1, X_batch2, y_phn_batch, y_spk_batch = map(Variable,
+                                                               bacth_els)
+            yield X_batch1, X_batch2, y_phn_batch, y_spk_batch
+
+    def train(self):
+        """Train method to train the model
+
+        """
+        patience_dev = 0
+        best_dev = None
+
+        self.train_losses = []
+        self.dev_losses = []
+
+        features, align_features, feat_dim = read_feats(self.feature_path)
+        train_loss = 0.0
+        dev_loss = 0.0
+        self.network.eval()
+        self.network.save_network()
+
+        self.network.train()
+        num_batches_train = 0
+        for minibatch in self.get_batches(features, train_mode=True):
+            # TODO refactor here for a step function based on specific loss
+            # enable generic train
+
+            X_batch1, X_batch2, y_phn_batch, y_spk_batch = minibatch
+            if self.cuda:
+                X_batch1 = X_batch1.cuda()
+                X_batch2 = X_batch2.cuda()
+                y_phn_batch = y_phn_batch.cuda()
+                y_spk_batch = y_spk_batch.cuda()
+
+            emb_batch1, emb_batch2 = self.network(X_batch1, X_batch2)
+            train_loss_value = self.loss(emb_batch1, emb_batch2,
+                                         y_phn_batch, y_spk_batch)
+            train_loss += train_loss_value.data[0]
+
+            num_batches_train += 1
+
+        self.train_losses.append(train_loss)
+        self.network.eval()
+        num_batches_dev = 0
+        for minibatch in self.get_batches(features, train_mode=False):
+            X_batch1, X_batch2, y_phn_batch, y_spk_batch = minibatch
+            if self.cuda:
+                X_batch1 = X_batch1.cuda()
+                X_batch2 = X_batch2.cuda()
+                y_phn_batch = y_phn_batch.cuda()
+                y_spk_batch = y_spk_batch.cuda()
+
+            emb_batch1, emb_batch2 = self.network(X_batch1, X_batch2)
+            dev_loss_value = self.loss(emb_batch1, emb_batch2,
+                                       y_phn_batch, y_spk_batch)
+            dev_loss += dev_loss_value.data[0]
+
+            num_batches_dev += 1
+
+        self.dev_losses.append(dev_loss)
+        normalized_train_loss = train_loss/num_batches_train
+        normalized_dev_loss = dev_loss/num_batches_dev
+        print("  training loss:\t\t{:.6f}".format(normalized_train_loss))
+        print("  dev loss:\t\t\t{:.6f}".format(normalized_dev_loss))
+
+        for epoch in range(self.num_epochs):
+            train_loss = 0.0
+            dev_loss = 0.0
+            start_time = time.time()
+
+            self.network.train()
+            for minibatch in self.get_batches(features, train_mode=True):
+                # TODO refactor here for a step function based on specific loss
+                # enable generic train
+                X_batch1, X_batch2, y_phn_batch, y_spk_batch = minibatch
+                if self.cuda:
+                    X_batch1 = X_batch1.cuda()
+                    X_batch2 = X_batch2.cuda()
+                    y_phn_batch = y_phn_batch.cuda()
+                    y_spk_batch = y_spk_batch.cuda()
+
+                self.optimizer.zero_grad()
+                emb_batch1, emb_batch2 = self.network(X_batch1, X_batch2)
+                train_loss_value = self.loss(emb_batch1, emb_batch2, y_batch)
+                train_loss_value.backward()
+                self.optimizer.step()
+                train_loss += train_loss_value.data[0]
+
+            self.train_losses.append(train_loss/num_batches_train)
+
+            self.network.eval()
+            for minibatch in self.get_batches(features, train_mode=False):
+                X_batch1, X_batch2, y_phn_batch, y_spk_batch = minibatch
+                if self.cuda:
+                    X_batch1 = X_batch1.cuda()
+                    X_batch2 = X_batch2.cuda()
+                    y_phn_batch = y_phn_batch.cuda()
+                    y_spk_batch = y_spk_batch.cuda()
+
+                emb_batch1, emb_batch2 = self.network(X_batch1, X_batch2)
+                dev_loss_value = self.loss(emb_batch1, emb_batch2, y_batch)
+                dev_loss += dev_loss_value.data[0]
+
+            self.dev_losses.append(dev_loss/num_batches_dev)
+
+            print("Epoch {} of {} took {:.3f}s".format(
+                    epoch + 1, self.num_epochs, time.time() - start_time))
+
+            normalized_train_loss = train_loss/num_batches_train
+            normalized_dev_loss = dev_loss/num_batches_dev
+            print("  training loss:\t\t{:.6f}".format(normalized_train_loss))
+            print("  dev loss:\t\t\t{:.6f}".format(normalized_dev_loss))
+            if best_dev is None or dev_loss < best_dev:
+                best_dev = dev_loss
+                patience_dev = 0
+                print('Saving best model so far, epoch {}'.format(epoch+1))
+                self.network.save_network()
+                self.save_whoami()
+                self.best_epoch = epoch
+            else:
+                patience_dev += 1
+                if patience_dev > self.patience:
+                    print("No improvements after {} iterations, "
+                          "stopping now".format(self.patience))
+                    print('Finished Training')
+                    break
+
+        print('Saving best checkpoint network')
+
+        self.network.load_network(
+                network_path=self.network.output_path+'.pth')
+        print('The best epoch is the {}-th'.format(self.best_epoch))
+        print('The best train is the {}'.format(
+                self.train_losses[self.best_epoch]))
+        print('The best dev is the {}'.format(
+                self.dev_losses[self.best_epoch]))
+        print('Still Training but no more patience.')
+        print('Finished Training')
 
 
 if __name__ == '__main__':
 
-    sia = SiameseNetwork(input_dim=3,num_hidden_layers=2,hidden_dim=10,
-                     output_dim=19,p_dropout=0.1,
-                     activation_layer='sigmoid',
-                     batch_norm=True, output_path='/home/rachine/abnet3/exp')
+    sia = SiameseNetwork(input_dim=3, num_hidden_layers=2, hidden_dim=10,
+                         output_dim=19, p_dropout=0.1,
+                         activation_layer='sigmoid',
+                         batch_norm=True,
+                         output_path='/home/rachine/abnet3/exp')
     sam = SamplerClusterSiamese(already_done=True, directory_output=None)
     loss = coscos2()
     sia.save_network()
-    tra = TrainerSiamese(sam,sia,loss, optimizer_type='adam')
+    tra = TrainerSiamese(sam, sia, loss, optimizer_type='adam')
