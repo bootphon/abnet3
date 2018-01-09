e292073d7771c0c357ee1535138bbc05a5869a44
Rachine <riadrachid3@gmail.com>
2018-01-09 19:29:47 +0100

    new test for reading yaml file

--- abnet3/gridsearch.py
+++ abnet3/gridsearch.py
@@ -7,6 +7,8 @@ It will run a search optimization for the different parameters of the model
 
 """
 
+import yaml
+
 
 class GridSearch(object):
     """Class Model for Grid search
@@ -14,30 +16,35 @@ class GridSearch(object):
         Parameters
         ----------
         input_file : String
-            Path to clusters of words
-        grid_sampler : dict
-            dict for grid search parameters for sampler
-        grid_trainer : dict
-            dict for grid search parameters for trainer
-        grid_model : dict
-            dict for grid search parameters for model
-        grid_loss : dict
-            dict for grid search parameters for loss
-        directory_output : String
-            Path folder where train/dev pairs folder will be
-        seed : int
-            Seed
+            Path to yaml file for grid search
         num_jobs: int
             Number of jobs to use
         gpu_ids: List
             List of Gpu ids available for computation
 
     """
-    def __init__(self, grid_sampler=None, grid_model=None, grid_loss=None,
-                 grid_trainer=None, directory_output=None, seed=0,
+    def __init__(self, input_file=None,
                  num_jobs=1, gpu_ids=None):
         super(GridSearch, self).__init__()
+        self.input_file = input_file
+        self.num_jobs = num_jobs
+        self.gpu_ids = gpu_ids
 
     def whoami(self):
         raise NotImplementedError('Unimplemented whoami for class:',
                                   self.__class__.__name__)
+
+    def parse_yaml_input_file(self):
+        """Parse yaml input file for grid search
+
+        """
+        with open(self.input_file, 'r') as stream:
+            try:
+                self.grid_params = yaml.load(stream)
+            except yaml.YAMLError as exc:
+                print(exc)
+
+
+if __name__ == '__main__':
+    grid = GridSearch(input_file='test/data/empty.yaml')
+    grid.parse_yaml_input_file()
