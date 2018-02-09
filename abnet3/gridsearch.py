e189aab8e5abd4a14e533e5401426f638d50f563
Rachine <riadrachid3@gmail.com>
2018-02-09 19:13:09 +0100

    little update for grid search

--- abnet3/gridsearch.py
+++ abnet3/gridsearch.py
@@ -9,6 +9,12 @@ It will run a search optimization for the different parameters of the model
 
 import yaml
 import faulthandler
+import itertools
+import re
+import os
+from path import Path
+import time
+import copy
 faulthandler.enable()
 
 
@@ -23,14 +29,17 @@ class GridSearch(object):
             Number of jobs to use
         gpu_ids: List
             List of Gpu ids available for computation
+        output_dir: String
+            Path to directory for the outputs of the grid search
 
     """
     def __init__(self, input_file=None,
-                 num_jobs=1, gpu_ids=None):
+                 num_jobs=1, gpu_ids=None, output_dir=None):
         super(GridSearch, self).__init__()
         self.input_file = input_file
         self.num_jobs = num_jobs
         self.gpu_ids = gpu_ids
+        self.output_dir = output_dir
 
     def whoami(self):
         raise NotImplementedError('Unimplemented whoami for class:',
@@ -46,14 +55,37 @@ class GridSearch(object):
             except yaml.YAMLError as exc:
                 print(exc)
 
+    def build_single_experiment(self):
+        """Build a single experiment from a dictionnary
+
+        """
+
     def build_grid_experiments(self):
-        """Extract the several list of experiments
+        """Extract the list of experiments to build the
 
         """
         self.parse_yaml_input_file()
-        experience_name = self.params.keys()
-        import pdb
-        pdb.set_trace()
+        assert self.params['default_params'], 'Yaml not well formatted'
+        assert self.params['grid_params'], 'Yaml not well formatted'
+
+        default_params = self.params['default_params']
+        grid_params = self.params['grid_params']
+        grid_experiments = []
+        for key in grid_params.keys():
+            current_exp = copy.deepcopy(default_params)
+            current_exp[key]
+            import pdb
+            pdb.set_trace()
+
+    def run(self):
+        """Run command to launch the grid search
+
+        """
+
+    def make_html(self):
+        """Build HTML outputs
+
+        """
 
 
 if __name__ == '__main__':
