0e6f77494e54d5a241c4b1c28f8bc855e68ea0a5
Rachine <riadrachid3@gmail.com>
2018-02-14 15:42:54 +0100

    test build_grid_experiment

--- test/test_gridsearch.py
+++ test/test_gridsearch.py
@@ -14,13 +14,31 @@ class TestGridsearch:
         grid = GridSearch(input_file=file_name)
 
         grid.parse_yaml_input_file()
-        dic_yaml = {'test_experiments_yaml': {
+        dic_yaml = {'default_params': {
+            'pathname_experience': '/empty_path',
             'features': {
-                'train': {
-                    'feature_path': None},
-                'test': {
-                    'feature_path': None}}}}
-        assert grid.grid_params == dic_yaml
+                'already_done': True,
+                'class': 'FeaturesGenerator'
+            }
+            },
+            'grid_params': {
+                'sampler': {
+                    'type_sampling_mode': ['1', 'log', 'f3', 'f', 'f2']
+                }
+            }
+            }
+
+        assert grid.params == dic_yaml
+
+    def test_build_grid_experiments(self):
+        base_path = os.path.dirname(__file__)
+        file_name = os.path.join(base_path, "data/empty.yaml")
+        grid = GridSearch(input_file=file_name)
+
+        grid_experiments = grid.build_grid_experiments()
+        assert len(grid_experiments) == 5
+        assert grid_experiments[0]['sampler']['type_sampling_mode'] == '1'
+        assert grid_experiments[1]['sampler']['type_sampling_mode'] == 'log'
 
 
 if __name__ == '__main__':
