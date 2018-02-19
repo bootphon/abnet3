c70389f3f828e6215c3805b09331b14447fc06a0
Rachine <riadrachid3@gmail.com>
2018-02-19 11:07:00 +0100

    Generic grid search

--- abnet3/gridsearch.py
+++ abnet3/gridsearch.py
@@ -45,7 +45,6 @@ class GridSearchBuilder(object):
     """
     def __init__(self, input_file=None,
                  num_jobs=1, gpu_ids=None, output_dir=None):
-        super(GridSearchBuilder, self).__init__()
         self.input_file = input_file
         self.num_jobs = num_jobs
         self.gpu_ids = gpu_ids
@@ -81,13 +80,13 @@ class GridSearchBuilder(object):
         current_exp = copy.deepcopy(default_params)
         now = datetime.datetime.now()
         for submodule, submodule_params in grid_params.items():
-            for param, values in submodule_params.items():
+            for param, values in submodule_params['arguments'].items():
                 for value in values:
                     try:
-                        current_exp[submodule][param] = value
+                        current_exp[submodule]['arguments'][param] = value
                     except Exception as e:
-                        current_exp[submodule] = {}
-                        current_exp[submodule][param] = value
+                        current_exp[submodule]['arguments'] = {}
+                        current_exp[submodule]['arguments'][param] = value
                     current_exp['pathname_experience'] = os.path.join(
                         current_exp['pathname_experience'] + now.isoformat(),
                         param,
@@ -111,7 +110,7 @@ class GridSearchBuilder(object):
         """
         grid_experiments = self.build_grid_experiments()
         print('Start the grid search ...')
-        for index in len(grid_experiments):
+        for index in range(len(grid_experiments)):
             pathname_exp = grid_experiments[index]['pathname_experience']
             exp_name = 'Starting exp {} : {}'.format(index, pathname_exp)
             self.run_single_experiment(
@@ -119,12 +118,12 @@ class GridSearchBuilder(object):
                 )
 
 
-class GridSearchSiamese(TrainerBuilder):
+class GridSearchSiamese(GridSearchBuilder):
     """Siamese Trainer class for ABnet3
 
     """
     def __init__(self, *args, **kwargs):
-        super(GridSearchBuilder, self).__init__(*args, **kwargs)
+        super(GridSearchSiamese, self).__init__(*args, **kwargs)
 
     def run_single_experiment(self, single_experiment=None, gpu_id=0):
         """Build a single experiment from a dictionnary of parameters
@@ -138,93 +137,58 @@ class GridSearchSiamese(TrainerBuilder):
         assert single_experiment['loss'], 'loss properties missing'
 
         features_prop = single_experiment['features']
-        features = getattr(abnet3.features, features_prop['class'])(
-             files=features_prop['files'],
-             output_path=features_prop['output_path'],
-             load_mean_variance_path=features_prop['load_mean_variance_path'],
-             save_mean_variance_path=features_prop['save_mean_variance_path'],
-             vad_folder=features_prop['vad_folder'],
-             n_filters=features_prop['n_filters'],
-             method=features_prop['method'],
-             normalization=features_prop['normalization'],
-             norm_per_file=features_prop['norm_per_file'],
-             stack=features_prop['stack'],
-             nframes=features_prop['nframes'],
-             deltas=features_prop['deltas'],
-             deltasdeltas=features_prop['deltasdeltas']
-        )
+        features_class = getattr(abnet3.features, features_prop['class'])
+        arguments = features_prop['arguments']
+        features = features_class(**arguments)
 
         sampler_prop = single_experiment['sampler']
-        sampler = getattr(abnet3.sampler, sampler_prop['class'])(
-            std_file=sampler_prop['std_file'],
-            spk_list_file=sampler_prop['spk_list_file'],
-            spkid_file=sampler_prop['spkid_file'],
-            batch_size=sampler_prop['batch_size'],
-            seed=sampler_prop['seed'],
-            type_sampling_mode=sampler_prop['type_sampling_mode'],
-            spk_sampling_mode=sampler_prop['spk_sampling_mode'],
-            ratio_same_diff_spk=sampler_prop['ratio_same_diff_spk'],
-            ratio_train_dev=sampler_prop['ratio_train_dev'],
-            ratio_same_diff_type=sampler_prop['ratio_same_diff_type'],
-            max_size_cluster=sampler_prop['max_size_cluster'],
-            num_total_sampled_pairs=sampler_prop['num_total_sampled_pairs'],
-            sample_batches=sampler_prop['sample_batches'],
-            directory_output=os.path.join(
-                 single_experiment['pathname_experience'], 'pairs')
-        )
+        sampler_class = getattr(abnet3.sampler, sampler_prop['class'])
+        arguments = sampler_prop['arguments']
+
+        arguments['directory_output'] = os.path.join(
+             single_experiment['pathname_experience'], 'pairs')
+        sampler = sampler_class(**arguments)
 
         model_prop = single_experiment['model']
-        model = getattr(abnet3.model, model_prop['class'])(
-            input_dim=model_prop['input_dim'],
-            num_hidden_layers=model_prop['num_hidden_layers'],
-            hidden_dim=model_prop['hidden_dim'],
-            output_dim=model_prop['output_dim'],
-            p_dropout=model_prop['p_dropout'],
-            activation_layer=model_prop['activation_layer'],
-            batch_norm=model_prop['batch_norm'],
-            output_path=os.path.join(
-                 single_experiment['pathname_experience'], 'network.pth')
-        )
+        model_class = getattr(abnet3.model, model_prop['class'])
+        arguments = model_prop['arguments']
+        arguments['output_path'] = os.path.join(
+             single_experiment['pathname_experience'], 'network.pth')
+        model = model_class(**arguments)
 
         loss_prop = single_experiment['loss']
-        loss = getattr(abnet3.loss, loss_prop['class'])(
-            avg=loss_prop['avg']
-        )
+        loss_class = getattr(abnet3.loss, loss_prop['class'])
+        arguments = loss_prop['arguments']
+        loss = loss_class(**arguments)
 
         dataloader_prop = single_experiment['dataloader']
-        dataloader = getattr(abnet3.dataloader, dataloader_prop['class'])(
-            pairs_path=sampler.directory_output,
-            features_path=features.output_path,
-            num_max_minibatches=dataloader_prop['num_max_minibatches'],
-            batch_size=dataloader_prop['batch_size'],
-            seed=dataloader_prop['seed']
-        )
+        dataloader_class = getattr(abnet3.dataloader, dataloader_prop['class'])
+        arguments = dataloader_prop['arguments']
+        arguments['pairs_path'] = sampler.directory_output
+        arguments['features_path'] = features.output_path
+        dataloader = dataloader_class(**arguments)
 
         trainer_prop = single_experiment['trainer']
-        trainer = getattr(abnet3.trainer, trainer_prop['class'])(
-            network=model,
-            loss=loss,
-            dataloader=dataloader,
-            cuda=trainer_prop['cuda'],
-            num_epochs=trainer_prop['num_epochs'],
-            lr=trainer_prop['lr'],
-            patience=trainer_prop['patience'],
-            optimizer_type=trainer_prop['optimizer_type'],
-            log_dir=os.path.join(
-                 single_experiment['pathname_experience'],
-                 'logs')
-        )
+        trainer_class = getattr(abnet3.trainer, trainer_prop['class'])
+        arguments = trainer_prop['arguments']
+        arguments['network'] = model
+        arguments['loss'] = loss
+        arguments['dataloader'] = dataloader
+        arguments['log_dir'] = os.path.join(
+             single_experiment['pathname_experience'],
+             'logs')
+        trainer = trainer_class(**arguments)
 
         embedder_prop = single_experiment['embedder']
-        embedder = EmbedderSiamese(
-               network=model,
-               cuda=embedder_prop['cuda'],
-               output_path=os.path.join(
-                    single_experiment['pathname_experience'],
-                    'embeddings.h5f'),
-               feature_path=features.output_path,
-               network_path=model.output_path,
-        )
+        embedder_class = getattr(abnet3.embedder, embedder_prop['class'])
+        arguments = embedder_prop['arguments']
+        arguments['network'] = model
+        arguments['output_path'] = os.path.join(
+             single_experiment['pathname_experience'],
+             'embeddings.h5f')
+        arguments['feature_path'] = features.output_path
+        arguments['network_path'] = model.output_path
+        embedder = embedder_class(**arguments)
 
         if features.already_done:
             pass
@@ -243,5 +207,3 @@ class GridSearchSiamese(TrainerBuilder):
 if __name__ == '__main__':
     grid = GridSearchSiamese(input_file='test/data/buckeye.yaml')
     grid.run()
-    # import pdb
-    # pdb.set_trace()
