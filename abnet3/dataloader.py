7abeed3be97292ce58f5d36ae22103e700250b10
DiegoAndai <dfandai@uc.cl>
2018-03-04 21:40:48 +0100

    Changed multimodal dataloader to frames

--- abnet3/dataloader.py
+++ abnet3/dataloader.py
@@ -470,11 +470,12 @@ class MultiTaskDataLoader(OriginalDataLoader):
                                                                batch_els)
             yield X_batch1, X_batch2, y_spk_batch, y_phn_batch
 
-class MultimodalDataLoader(OriginalDataLoader):
+class MultimodalDataLoader(FramesDataLoader):
     """
     Class to manage multiple inputs, extract features
     from features file and provide iterator for multimodal
-    training of the siamese network
+    training of the siamese network. Multimodal version
+    of the frames dataloader
     """
 
     def __init__(self, pairs_path, features_path, num_max_minibatches=1000, seed=None, batch_size=8):
@@ -546,105 +547,33 @@ class MultimodalDataLoader(OriginalDataLoader):
             dev_dir = os.path.join(self.pairs_path, 'dev_pairs/dataset')
             self.pairs['dev'] = read_dataset(dev_dir)
 
-    def load_frames_from_pairs(self, pairs, seed=0, fid2spk=None, set_alignment=False):
-        """Prepare a batch in Pytorch format based on a batch file
-        :param pairs: list of pairs under the form
-                      {'same': [pairs], 'diff': [pairs] }
-        :param seed: randomness
-        :param fid2spk:
-            if None, will return X1, X2, y_phones
-            If it is the spkid mapping, will return X1, X2, y_phones, y_speaker
-        :param set_alignmet:
-            if True, the alignment attribute will be recomputed
-        """
+        if self.token_features['train'] is None:
+            print("Loading all frames..", end='', flush=True)
+            self.token_features['train'], self.frame_pairs['train'] = \
+                self.load_all_frames(self.pairs['train'])
+            print("Done. %s frame pairs in total." % len(self.frame_pairs['train']))
 
-        #TODO: rethink alignment argument
+        if self.token_features['dev'] is None:
+            self.token_features['dev'], self.frame_pairs['dev'] = \
+                self.load_all_frames(self.pairs['dev'])
 
-        # f are filenames, s are start times, e are end times
-        token_feats = {}
-        for f1, s1, e1, f2, s2, e2 in pairs['same']:
-            if (f1, s1, e1) not in token_feats:
-                token_feats[f1, s1, e1] = self.features.get(f1, s1, e1)
-            if (f2, s2, e2) not in token_feats:
-                token_feats[f2, s2, e2] = self.features.get(f2, s2, e2)
-        for f1, s1, e1, f2, s2, e2 in pairs['diff']:
-            if (f1, s1, e1) not in token_feats:
-                token_feats[f1, s1, e1] = self.features.get(f1, s1, e1)
-            if (f2, s2, e2) not in token_feats:
-                token_feats[f2, s2, e2] = self.features.get(f2, s2, e2)
 
-        # 2. align features for each pair
-        X1, X2, y_phn, y_spk = [], [], [], []
-        # get features for each same pair based on DTW alignment paths
-        for f1, s1, e1, f2, s2, e2 in pairs['same']:
-            if (s1 > e1) or (s2 > e2):
-                continue
-            feat1 = token_feats[f1, s1, e1]
-            feat2 = token_feats[f2, s2, e2]
-            try:
-                if set_alignment:
-                    path1, path2 = get_dtw_alignment(feat1, feat2)
-                    self.alignment_dict[f1, s1, e1, f2, s2, e2] = (path1, path2)
-                else:
-                    path1, path2 = self.alignment_dict[f1, s1, e1, f2, s2, e2]
-            except Exception:
-                continue
+    def load_all_frames(self, pairs):
+        token_feats_list = [] #list of token feats for every modality
+        self.features = self.features_dict[self.features_path[0]]
+        token_feats, frames = super(MultimodalDataLoader, self).load_all_frames(pairs)
+                              #loads token feats, alignment and
+                              #frames for first path
+        token_feats_list.append(token_feats_list)
 
-            self.statistics_training['SameType'] += 1
 
-            if fid2spk:
-                spk1, spk2 = fid2spk[f1], fid2spk[f2]
-                if spk1 is spk2:
-                    y_spk.append(np.ones(len(path1)))
-                    self.statistics_training['SameTypeSameSpk'] += 1
-                else:
-                    y_spk.append(-1 * np.ones(len(path1)))
-                    self.statistics_training['SameTypeDiffSpk'] += 1
+        for path in self.features_path[1:]: #add token feats of the other modalities
+                                            #to the token feats dict
+            self.features = self.features_dict[path]
+            path_token_feats = self.get_token_feats(pairs)
+            token_feats_list.append(path_token_feats)
 
-            X1.append(feat1[path1, :])
-            X2.append(feat2[path2, :])
-            y_phn.append(np.ones(len(path1)))
-
-        for f1, s1, e1, f2, s2, e2 in pairs['diff']:
-            if (s1 > e1) or (s2 > e2):
-                continue
-            feat1 = token_feats[f1, s1, e1]
-            feat2 = token_feats[f2, s2, e2]
-            n1 = feat1.shape[0]
-            n2 = feat2.shape[0]
-            X1.append(feat1[:min(n1, n2), :])
-            X2.append(feat2[:min(n1, n2), :])
-            y_phn.append(-1 * np.ones(min(n1, n2)))
-
-            self.statistics_training['DiffType'] += 1
-
-            if fid2spk:
-                spk1, spk2 = fid2spk[f1], fid2spk[f2]
-                if spk1 is spk2:
-                    y_spk.append(np.ones(min(n1, n2)))
-                    self.statistics_training['DiffTypeSameSpk'] += 1
-                else:
-                    y_spk.append(-1 * np.ones(min(n1, n2)))
-                    self.statistics_training['DiffTypeDiffSpk'] += 1
-
-        if fid2spk:
-            assert len(y_phn) == len(y_spk), 'not same number of labels...'
-
-        # concatenate all features
-        X1, X2, y_phn = np.vstack(X1), np.vstack(X2), np.concatenate(y_phn)
-        np.random.seed(seed)
-        n_pairs = len(y_phn)
-
-        ind = np.random.permutation(n_pairs)
-        X1 = X1[ind, :]
-        X2 = X2[ind, :]
-        y_phn = y_phn[ind]
-
-        if fid2spk:
-            y_spk = np.concatenate(y_spk)[ind]
-            return X1, X2, y_spk, y_phn
-
-        return X1, X2, y_phn
+        return token_feats_list, frames
 
 
     def batch_iterator(self, train_mode=True):
@@ -660,12 +589,10 @@ class MultimodalDataLoader(OriginalDataLoader):
         X2list are lists of torch variables, each one corresponding
         to the different modes representing the same phenomena,
         and y is a torch variable which contains same/different info, which is
-        the same for every mode
+        the same for every modality
         """
 
-        #TODO: support unsampled batches
-
-        #load pairs and features
+        # read all features
         self.load_data()
 
         if train_mode:
@@ -673,38 +600,41 @@ class MultimodalDataLoader(OriginalDataLoader):
         else:
             mode = 'dev'
 
-        pairs = self.pairs[mode]
-        num_pairs = len(pairs)
+        frame_pairs = self.frame_pairs[mode]
+        num_pairs = len(frame_pairs)
+        num_batches = num_pairs // self.batch_size
 
-        sliced_indexes = range(0, num_pairs, self.batch_size)
-        batches = [pairs[idx:idx + self.batch_size] for idx in sliced_indexes]
-        num_batches = len(batches)
+        if num_batches == 0:
+            num_batches = 1
 
-        if self.num_max_minibatches < num_batches:
-            selected_batches = np.random.choice(range(num_batches),
-                                                self.num_max_minibatches,
-                                                replace=False)
+        # choose which batches to run in this epoch
+        if mode == 'dev' or self.max_batches_per_epoch is None:  # normal behaviour
+            batch_ids = range(num_batches)
+            if self.randomize_dataset:
+                np.random.shuffle(frame_pairs)
         else:
-            print("Number of batches not sufficient," +
-                  " iterating over all the batches")
-            selected_batches = np.random.permutation(range(num_batches))
+            # we want to read only a subset of the dataset
+            if self.batch_position >= num_batches:  # reset the count
+                print("Arrived at the end of the dataset. Starting over.")
+                if self.randomize_dataset:
+                    np.random.shuffle(frame_pairs)
+                self.batch_position = 0
+            batch_ids = range(
+                self.batch_position,
+                min(self.batch_position + self.max_batches_per_epoch,
+                    num_batches)
+            )
+            self.batch_position += self.max_batches_per_epoch
 
-        #Yield batches
-        for batch_id in selected_batches:
-            grouped_pairs = group_pairs(batches[batch_id])
-            X1list, X2list = [], []
-            set_alignment = True
-            for path in self.features_path:
-                self.features = self.features_dict[path]
-                batch_els = self.load_frames_from_pairs(grouped_pairs,
-                                                        set_alignment=set_alignment)
-                batch_els = map(torch.from_numpy, batch_els)
-                X_batch1, X_batch2, y_batch = map(Variable, batch_els)
-                X1list.append(X_batch1)
-                X2list.append(X_batch2)
-                set_alignment = False #Only set alignment with the first path's features
+        for i in batch_ids:
+            pairs_batch = frame_pairs[i*self.batch_size:
+                                      i*self.batch_size + self.batch_size]
 
-            assert len(X1list[0]) == len(X2list[0]), "Dimension error on batch construction"
-            assert len(X1list[0]) == len(y_batch), "Dimension error on batch construction"
-
-            yield X1list, X2list, y_batch
+            X1_list = []
+            X2_list = []
+            for token_features in self.token_features[mode]:
+                X1, X2, y = self.load_batch(pairs_batch, token_features)
+                X1_list.append(Variable(torch.from_numpy(X1), volatile=not train_mode))
+                X2_list.append(Variable(torch.from_numpy(X2), volatile=not train_mode))
+                y_torch = Variable(torch.from_numpy(y), volatile=not train_mode)
+            yield X1_list, X2_list, y_torch
