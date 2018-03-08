2fe618a6ae629124290a350e2aa103fb7c41753f
DiegoAndai <dfandai@uc.cl>
2018-03-08 17:27:21 +0100

    Typo on IntegrationBuilder

--- abnet3/integration.py
+++ abnet3/integration.py
@@ -25,7 +25,7 @@ class IntegrationUnitBuilder(nn.Module):
     Base class for integration units
     """
 
-    def __init__(self, cuda_bool=False *args, **kwargs):
+    def __init__(self, cuda_bool=False, *args, **kwargs):
         super(IntegrationUnitBuilder, self).__init__()
 
         self.cuda_bool = cuda_bool
