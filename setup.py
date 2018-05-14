4aeba6f285b85fb541754fbf94653f0350eb24cf
Corentin <corentin@cdancette.fr>
2018-05-14 17:36:11 +0200

    correct path to cli

--- setup.py
+++ setup.py
@@ -44,6 +44,6 @@ setup(
     },
     entry_points={'console_scripts': [
         'abnet3-gridsearch = abnet3.gridsearch:main',
-        'abnet3-embed = abnet3.embed_cli:main',
+        'abnet3-embed = abnet3.tools.embed_cli:main',
     ]}
 )
