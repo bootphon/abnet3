9d5ecb60eab64f26cfe7943a42540fbf60fb3e3d
Mathieu Bernard <mathieu.a.bernard@inria.fr>
2018-10-12 16:57:28 +0200

    test_install compatible with pip >= 10

--- test/test_install.py
+++ test/test_install.py
@@ -19,10 +19,19 @@ def test_requirements():  # pylint: disable=no-self-use
     This implementation is tested to be compatible with pip 9.0.1.
     Test stolen from stack-overflow
     """
+    try: # for pip >= 10
+        from pip._internal.req import parse_requirements
+    except ImportError: # for pip <= 9.0.3
+        from pip.req import parse_requirements
+
+    try:
+        session = pip.download.PipSession()
+    except AttributeError:  # for pip >= 10
+        session = pip._internal.download.PipSession()
+
     requirements_path = pathlib.Path(__file__).parents[1].joinpath(
                         'requirements.txt')
-    requirements = pip.req.parse_requirements(
-                   str(requirements_path), session=pip.download.PipSession())
+    requirements = parse_requirements(str(requirements_path), session=session)
     requirements = [str(r.req) for r in requirements]
     requirements = [r for r in requirements if r != 'None']
     pkg_resources.require(requirements)
